3e266e0901e64ba45aeb1a736835610f8934f824
Oscar Mauricio <dev_deep0@proton.me>
2024-10-26 15:18:48 -0600

    Agragar Clase de conexion a la BD

---
+++ ISNP001724_Bloque2/ISNP001724_Bloque2/db_conexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ISNP001724_Bloque2
+{
+    internal class db_conexion
+    {
+        SqlConnection miConexion = new SqlConnection();
+        SqlCommand miComando = new SqlCommand();
+        SqlDataAdapter miAdaptador = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+        public db_conexion()
+        {
+            miConexion.ConnectionString = "@Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\peliculas.mdf;Integrated Security=True";
+        }
+        public DataSet obtenerDatos() { 
+            miComando.Connection = miConexion;
+            miComando.CommandText = "SELECT * FROM peliculas";
+            miAdaptador.Fill(ds, "peliculas");
+
+            return ds; }
+    }
+
+    
+}
