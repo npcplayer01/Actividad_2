cb7ef3c84c9400cbd75979b96b19f8e1613f96ee
Oscar Mauricio <dev_deep0@proton.me>
2024-10-27 20:14:08 -0600

    cambios de imagen

--- ISNP001724_Bloque2/ISNP001724_Bloque2/Form1.Designer.cs
+++ ISNP001724_Bloque2/ISNP001724_Bloque2/Form1.Designer.cs
@@ -50,6 +50,7 @@
             this.btnBorrar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnNuevo = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.grbDatos.SuspendLayout();
             this.grbNavegacion.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbFinal)).BeginInit();
@@ -57,6 +58,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbAtras)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPrincipio)).BeginInit();
             this.grbEdicion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // grbDatos
@@ -270,11 +272,22 @@
             this.btnNuevo.UseVisualStyleBackColor = true;
             this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(323, 405);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1553, 577);
+            this.ClientSize = new System.Drawing.Size(810, 577);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.grbEdicion);
             this.Controls.Add(this.grbNavegacion);
             this.Controls.Add(this.grbDatos);
@@ -291,7 +304,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbAtras)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPrincipio)).EndInit();
             this.grbEdicion.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -318,6 +333,7 @@
         private System.Windows.Forms.Button btnBorrar;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
